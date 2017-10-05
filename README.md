59df013268aef133cb522ea7725d46f52f2e6a6d
Emma Drueke <30911953+emmadrueke@users.noreply.github.com>
2017-10-05 16:46:10 -0500

    Add files via upload
    Writing prompts are completed.

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
-# Homework #4
+# Homework #2
 
 ## Instructions
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
-	* Callback Functions
-	* Closure
-	* `arguments`
-	* Recursion
-	* `prototype`
-	* Constructors
+* `for`:
+	A ‘for’ loop is crucial and makes it possible to iterate over each position in a string, an array, and an object to evaluate the data that is at a specific position. 
+		
+* `&&`, `||`, `!`
+	These are just a couple of operators that can be used in ‘if’ statements or ‘for’ loops.
+		 &&: means ‘and. The argument that is given to the for loop needs to have both conditions an example is if the number passed from the argument needs to be an even number && be greater than 10. If the number is true for both of those conditions then the code will be told to do something like return true, or print true in the console. 
+		||: means ‘or’ it is used similarly to && except that only one of the conditions needs to be true. Using the same example from the && definition, if some number that is passed from the function is even || greater then 10, return true. So only one of those conditions needs to be true in order for the code to continue running.
+		!: Means not. It reverses the truthiness of a statement. !true = false. !false = true. !10 = any number but 10. 
+
+* Arrays:
+	A list of data. So that could be a list of numbers, a list of strings which could be the names of all your favorite restaurants. It can also have different types of data sets in them like numbers and strings mixed together, but that’s not good practice. It can also be a list of objects, which we’ll talk about in the next homework assignment. 
+
+* Git:
+	Git is a place where you can manage you code and all of it’s previous versions. 
+		
+* GitHub:
+	GitHub is the place where you host your code and collaborate with others. 
+		
 
 
-2. Fork and clone this repo.  When you need to commit use the following commands.
+
+2. Install git.  https://git-scm.c]om/downloads
+
+
+3. Fork and clone this repo.  When you need to commit use the following commands.
 		
 	* `git status`
 	* `git add --all`
@@ -20,12 +36,10 @@
 	* `git commit -m "your commit message"`
 	* `git push origin master`
 
-3. Install dependencies using `npm install`.  Run tests using `npm test`.
 
 4. Make the tests pass!
 
 
-
-#### Congratulations on finishing Homework #4!
+#### Congratulations on finishing Homework #2!
 
 For more information about Lambda School's six month CS program visit: https://lambdaschool.com
--- exercises.js
+++ exercises.js
@@ -1,87 +1,209 @@
 // Do not change any of the function names
 
-function multiplyArguments() {
-  // use the arguments keyword to multiply all of the arguments together and return the product
-  // if no arguments are passed in return 0
-  // if one argument is passed in just return it
+function getBiggest(x, y) {
+  if ( x > y ) {
+    return x;
+  } else if (y > x){
+      return y;
+  } else {
+      return x || y;
+  }
+
+  // x and y are integers.  Return the larger integer
+  // if they are the same return either one
 }
 
-function invokeCallback(cb) {
-  // invoke cb
+
+function greeting(language) {
+  if (language === 'German') {
+    return 'Guten Tag!';
+  } else if(language === 'English') {
+      return 'Hello!';
+  } else if (language === 'Spanish') {
+      return 'Hola!';
+  } else {
+      return 'Hello!';
+  }
+  // return a greeting for three different languages:
+  // language: 'German' -> 'Guten Tag!'
+  // language: 'English' -> 'Hello!'
+  // language: 'Spanish' -> 'Hola!'
+  // if language is undefined return 'Hello!'
 }
 
-function sumArray(numbers, cb) {
-  // sum up all of the integers in the numbers array
-  // pass the result to cb
-  // no return is necessary
+function isTenOrFive(num) {
+  if (num === 10){
+    return true;
+  } else if (num === 5){
+      return true;
+  } else{
+      return false;
+  }
+  // return true if num is 10 or 5
+  // otherwise return false
 }
 
-function forEach(arr, cb) {
-  // iterate over arr and pass its values to cb one by one
-  // hint: you will be invoking cb multiple times (once for each value in the array)
+function isInRange(num) {
+  if (20 < num && num < 50){
+    return true;
+  } else {
+      return false;
+  }
+  // return true if num is less than 50 and greater than 20
 }
 
-function map(arr, cb) {
-  // create a new array
-  // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
-  // the new array should be the same length as the array argument
+function isInteger(num) {
+  if (num - Math.floor(num) === 0){
+    return true;
+  } else{
+      return false;
+  }
+  // return true if num is an integer
+  // 0.8 -> false
+  // 1 -> true
+  // -10 -> true
+  // otherwise return false
+  // hint: you can solve this using Math.floor
 }
 
-function getUserConstructor() {
-  // create a constructor called User
-  // it should accept an options object with username, name, email, and password properties
-  // in the constructor set the username, name, email, and password properties
-  // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
-  // {{name}} should be the name set on each instance
-  // return the constructor
+function fizzBuzz(num) {
+ 
+  if (num % 3 ===0 && num % 5 === 0){
+    return 'fizzbuzz';
+  } else if (num % 5 === 0){
+      return 'buzz';
+  } else if ( num % 3 === 0){
+      return 'fizz';
+  } else {
+      return num;
+  }
+  // if num is divisible by 3 return 'fizz'
+  // if num is divisible by 5 return 'buzz'
+  // if num is divisible by 3 & 5 return 'fizzbuzz'
+  // otherwise return num
 }
 
-function addPrototypeMethod(Constructor) {
-  // add a method to the constructor's prototype
-  // the method should be called 'sayHi' and should return the string 'Hello World!'
+function isPrime(num) {
+  if (num % 2 === 0){
+    return false;
+  } else if (num === 0 || num === 1){
+     return false;
+  }
+  for (var i = 3; i * i <= num; i += 2){
+    if (num % i === 0){
+      return false;
+    }
+  } return true;
+} 
+
+function returnFirst(arr) {
+  return arr[0];
+  // return the first item from the array
 }
 
-function addReverseString() {
-  // add a method to the string constructor's prototype that returns a reversed copy of the string
-  // name this method reverse
-  // hint:
-  // you will need to use 'this' inside of reverse
+function returnLast(arr) {
+  return arr[arr.length-1];
+  // return the last item of the array
 }
 
-function nFactorial(n) {
-  // return the factorial for n
-  // solve this recursively
-  // example:
-  // the factorial of 3 is 6 (3 * 2 * 1)
+function getArrayLength(arr) {
+  return arr.length;
+
+  // return the length of the array
 }
 
-function cacheFunction(cb) {
-  // Extra Credit
-  // use closure to create a cache for the cb function
-  // the function that you return should accept a single argument and invoke cb with that argument
-  // when the function you return is invoked with an argument it should save that argument and its result
-  // when the function you return is called again with an argument that it has seen before it should not call cb
-  // but should instead directly returned the previous result
-  // example:
-  // cb -> function(x) { return x * x; }
-  // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
-  // if the function you return is invoked again with 5 it will look on an object in the closure scope
-  // and return 25 directly and will not invoke cb again
+function incrementByOne(arr) {
+  for (var i=0; i < arr.length ; i++){
+    arr[i] += 1;
+  }
+  return arr;
+    
+
+  // arr is an array of integers  
+  // increase each integer by one
+  // return the array
 }
 
+function addItemToArray(arr, item) {
+  arr.push(item);
+  return arr;
+  // add the item to the end of the array
+  // return the array
+}
+
+function addItemToFront(arr, item) {
+  arr.unshift(item);
+  return arr;
+  // add the item to the front of the array
+  // return the array
+  // hint: use the array method .unshift
+}
+
+function wordsToSentence(words) {
+  var sentence = words.join(' ');
+  return sentence;
+}
+
+function contains(arr, item) {
+  for(var i = 0; i < arr.length; i++){
+    if (arr[i] === item){
+      return true;
+    }
+  }
+  return false;
+
+  // check to see if item is inside of arr
+  // return true if it is, otherwise return false
+}
+
+function addNumbers(numbers) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++){
+    sum = sum + numbers[i];
+
+  }return sum;
+
+  // numbers is an array of integers.
+  // add all of the integers and return the value
+}
+
+function averageTestScore(testScores) {
+  var sum = 0;
+  for (var i = 0; i < testScores.length; i++){
+    sum = sum + testScores[i];
+
+  }
+  return sum/testScores.length;
+  // testScores is an array.  Iterate over testScores and compute the average.
+  // return the average
+}
+
+function largestNumber(numbers) {
+  return Math.max.apply(null, numbers);
+  // numbers is an array of integers
+  // return the largest integer
+}
 
 // Do not modify code below this line.
 // --------------------------------
 
 module.exports = {
-  multiplyArguments: multiplyArguments,
-  invokeCallback: invokeCallback,
-  sumArray: sumArray,
-  forEach: forEach,
-  map: map,
-  getUserConstructor: getUserConstructor,
-  addPrototypeMethod: addPrototypeMethod,
-  addReverseString: addReverseString,
-  nFactorial: nFactorial,
-  cacheFunction: cacheFunction
+  getBiggest: getBiggest,
+  greeting: greeting,
+  isTenOrFive: isTenOrFive,
+  isInRange: isInRange,
+  isInteger: isInteger,
+  fizzBuzz: fizzBuzz,
+  isPrime: isPrime,
+  returnFirst: returnFirst,
+  returnLast: returnLast,
+  getArrayLength: getArrayLength,
+  incrementByOne: incrementByOne,
+  addItemToArray: addItemToArray,
+  addItemToFront: addItemToFront,
+  wordsToSentence: wordsToSentence,
+  contains: contains,
+  addNumbers: addNumbers,
+  averageTestScore: averageTestScore,
+  largestNumber: largestNumber
 };
